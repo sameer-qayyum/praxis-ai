5000f9ca71a0425763befbd58a6cc820fc8c0369
Sameer Tahir <sameerqayyum@hotmail.com>
2025-09-11 13:20:32 +1000

    Redirect to new url

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = process.env.NEXT_PUBLIC_SITE_URL || 
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
